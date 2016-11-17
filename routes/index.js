c8dcc2878fc111a1960d05885144530209e72671
Lucero <lucero.lara21@gmail.com>
2016-11-17 12:56:24 -0800

    cambio ../log

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var express = require('express')
+var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var Account = require('../models/account');
@@ -19,7 +19,7 @@ router.config = App.new(__dirname + "/app.json");
 var sequence = 0;
 var temp = new Date();
 var date_str = temp.getFullYear() + "-" + (1 + temp.getMonth()) + "-"+ temp.getDate();
-var log_dir = process.env.OPENSHIFT_DATA_DIR || "./log";
+var log_dir = process.env.OPENSHIFT_DATA_DIR || "../log";
 
 // create logger to console and file
 var logger = new (winston.Logger)({
